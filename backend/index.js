1386868d56ff3e418fffe0f19b9259708165d949
José Núñez <79015124+josiext@users.noreply.github.com>
2022-05-15 01:49:09 -0400

    Implement npm workspaces

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,36 @@ import Koa from "koa";
 
 const app = new Koa();
 
+const TODO_CACHE = [];
+
+const get = () => {
+  return TODO_CACHE;
+};
+
+const post = (data) => {
+  TODO_CACHE.push(data);
+  return data;
+};
+
+const update = (data) => {
+  return data;
+};
+
+const remove = (id) => {
+  return { id: "123", label: "123" };
+};
+
 app.use(async (ctx) => {
-  ctx.body = "Hello World";
+  if (ctx.method === "GET") ctx.body = get();
+  if (ctx.method === "POST") ctx.body = post();
+  if (ctx.method === "UPDATE") ctx.body = update();
+  if (ctx.method === "DELETE") ctx.body = remove();
 });
 
-app.listen(3000);
+app.on("error", (err) => {
+  console.log("server error", err);
+});
+
+app.listen(3001);
+
+console.log("Backend on http://localhost:3001/");
